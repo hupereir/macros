76cd47ecdf714e74b9464430a142bb6b6ee70e22
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2019-11-05 17:50:33 -0500

    added macros to create and readback geant DSTs

---
+++ macros/g4simulations/Fun4All_G4_CreateDST_hp.C
@@ -0,0 +1,167 @@
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllDummyInputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllNoSyncDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <g4main/PHG4ParticleGeneratorBase.h>
+#include <g4main/PHG4ParticleGenerator.h>
+#include <g4main/PHG4SimpleEventGenerator.h>
+#include <g4main/PHG4ParticleGeneratorVectorMeson.h>
+#include <g4main/PHG4ParticleGun.h>
+#include <g4main/HepMCNodeReader.h>
+#include <g4detectors/PHG4DetectorSubsystem.h>
+#include <phool/recoConsts.h>
+#include <phpythia6/PHPythia6.h>
+#include <phpythia8/PHPythia8.h>
+#include <phhepmc/Fun4AllHepMCPileupInputManager.h>
+#include <phhepmc/Fun4AllHepMCInputManager.h>
+
+R__ADD_INCLUDE_PATH( /phenix/u/hpereira/sphenix/src/macros/macros/g4simulations )
+#include "G4Setup_sPHENIX.C"
+#include "G4_Bbc.C"
+#include "G4_Global.C"
+#include "G4_CaloTrigger.C"
+#include "G4_Jets.C"
+#include "G4_HIJetReco.C"
+#include "G4_DSTReader.C"
+#include "DisplayOn.C"
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libg4testbench.so)
+R__LOAD_LIBRARY(libphhepmc.so)
+R__LOAD_LIBRARY(libPHPythia6.so)
+R__LOAD_LIBRARY(libPHPythia8.so)
+#endif
+
+int Fun4All_G4_CreateDST_hp( const int nEvents = 5000, const char *outputFile = "DST/dst.root" )
+{
+
+  //===============
+  // Input options
+  //===============
+
+  //======================
+  // What to run
+  //======================
+
+  bool do_bbc = true;
+  bool do_pipe = true;
+
+  bool do_tracking = true;
+  bool do_tracking_cell = do_tracking && true;
+  bool do_tracking_track = do_tracking_cell && true;
+  bool do_tracking_eval = do_tracking_track && true;
+
+  bool do_pstof = false;
+  bool do_cemc = false;
+  bool do_hcalin = false;
+  bool do_magnet = false;
+  bool do_hcalout = false;
+  bool do_plugdoor = false;
+
+  // establish the geometry and reconstruction setup
+  G4Init(do_tracking, do_pstof, do_cemc, do_hcalin, do_magnet, do_hcalout, do_pipe, do_plugdoor);
+
+  // set to 1 to make all absorbers active volumes
+  int absorberactive = 1;
+
+  // default map from the calibration database
+  // scale the map to a 1.4 T field
+  const auto magfield = std::string(getenv("CALIBRATIONROOT")) + std::string("/Field/Map/sPHENIX.2d.root");
+  const float magfield_rescale = -1.4 / 1.5;
+
+  //---------------
+  // Fun4All server
+  //---------------
+
+  auto se = Fun4AllServer::instance();
+  se->Verbosity(0);
+  auto rc = recoConsts::instance();
+
+  //-----------------
+  // Event generation
+  //-----------------
+
+  {
+    // toss low multiplicity dummy events
+    auto gen = new PHG4SimpleEventGenerator();
+    gen->add_particles("pi+",1);
+
+    gen->set_vertex_distribution_function(
+      PHG4SimpleEventGenerator::Uniform,
+      PHG4SimpleEventGenerator::Uniform,
+      PHG4SimpleEventGenerator::Uniform);
+
+    gen->set_vertex_distribution_mean(0.0, 0.0, 0.0);
+    gen->set_vertex_distribution_width(0.0, 0.0, 5.0);
+
+    // TODO: what are vertex_size
+    gen->set_vertex_size_function(PHG4SimpleEventGenerator::Uniform);
+    gen->set_vertex_size_parameters(0.0, 0.0);
+    gen->set_eta_range(-1.0, 1.0);
+    gen->set_phi_range(-1.0 * TMath::Pi(), 1.0 * TMath::Pi());
+    gen->set_pt_range(0.1, 20.0);
+    gen->Embed(2);
+    gen->Verbosity(0);
+
+    se->registerSubsystem(gen);
+  }
+
+  {
+    // G4 setup
+    G4Setup(
+      absorberactive, magfield, EDecayType::kAll,
+      do_tracking, do_pstof, do_cemc, do_hcalin, do_magnet, do_hcalout, do_pipe, do_plugdoor,
+      magfield_rescale);
+  }
+
+  {
+    // bbc reconstruction
+    BbcInit();
+    Bbc_Reco();
+  }
+
+  {
+    // tracking
+    Tracking_Cells();
+    Tracking_Reco();
+  }
+
+  {
+    // for single particle generators we just need something which drives
+    // the event loop, the Dummy Input Mgr does just that
+    auto in = new Fun4AllDummyInputManager("JADE");
+    se->registerInputManager(in);
+  }
+
+  {
+    // output manager
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
+    out->AddNode("TRKR_HITSET");
+    out->AddNode("TRKR_CLUSTER");
+    out->AddNode("SvtxTrackMap");
+    se->registerOutputManager(out);
+  }
+
+  //-----------------
+  // Event processing
+  //-----------------
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  gSystem->Exit(0);
+  return 0;
+}
+
+// This function is only used to test if we can load this as root6 macro
+// without running into unresolved libraries and include files
+void RunFFALoadTest() {}
---
+++ macros/g4simulations/Fun4All_G4_ReadDST_hp.C
@@ -0,0 +1,57 @@
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <phool/recoConsts.h>
+
+#include <trackbase/TrkrDefs.h>
+#include <trackbase/TrkrCluster.h>
+#include <trackbase/TrkrClusterContainer.h>
+#include <trackbase/TrkrHit.h>
+
+#include <trackbase_historic/SvtxTrack.h>
+#include <trackbase_historic/SvtxTrackMap.h>
+
+// #include <intt/InttHit.h>
+// #include <mvtx/MvtxHit.h>
+// #include <outertracker/OuterTrackerHit.h>
+
+// custom evaluator
+#include <g4eval/TrackingEvaluator_hp.h>
+
+R__LOAD_LIBRARY(libg4eval.so)
+
+int Fun4All_G4_ReadDST_hp( const int nEvents = 0, const char* inputFile = "DST/dst_5k.root", const char *outputFile = "rootfiles/G4sPHENIX" )
+{
+
+  // server
+  auto se = Fun4AllServer::instance();
+  se->Verbosity(0);
+  auto rc = recoConsts::instance();
+
+  // evaluation
+  auto evaluator = new TrackingEvaluator_hp( "TRACKINGEVALUATOR_HP", std::string(outputFile) + "_g4svtx_eval.root" );
+  evaluator->Verbosity(0);
+  se->registerSubsystem(evaluator);
+
+  // input manager
+  auto *in = new Fun4AllDstInputManager("DSTin");
+  in->fileopen(inputFile);
+  se->registerInputManager(in);
+
+  //-----------------
+  // Event processing
+  //-----------------
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  gSystem->Exit(0);
+  return 0;
+}
+
+// This function is only used to test if we can load this as root6 macro
+// without running into unresolved libraries and include files
+void RunFFALoadTest() {}
