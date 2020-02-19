0e456c8ab75bfc2c66af837ce7a5e1ca82fdd371
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-02-19 11:57:47 -0500

    added macro to run clustering only and not tracking.

---
+++ macros/g4simulations/Fun4All_G4_Clustering_hp.C
@@ -0,0 +1,72 @@
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <phool/recoConsts.h>
+
+// own modules
+#include <g4eval/EventCounter_hp.h>
+#include <g4eval/TrackingEvaluator_hp.h>
+
+R__ADD_INCLUDE_PATH( /phenix/u/hpereira/sphenix/src/macros/macros/g4simulations )
+#include "G4Setup_sPHENIX.C"
+#include "G4_Bbc.C"
+
+R__LOAD_LIBRARY(libfun4all.so)
+
+//________________________________________________________________________________________________
+int Fun4All_G4_Clustering_hp(
+  const int nEvents = 0,
+  const char* inputFile = "DST/dst_sim.root",
+  const char *outputFile = "DST/dst_clusters.root" )
+{
+
+  // customize tpc
+  Tpc::enable_tpc_distortions = false;
+  Tpc::misalign_tpc_clusters = false;
+
+  // customize outer tracker
+  OuterTracker::n_outertrack_layers = 0;
+
+  // server
+  auto se = Fun4AllServer::instance();
+  se->Verbosity(0);
+
+  auto rc = recoConsts::instance();
+  rc->set_IntFlag("RANDOMSEED", 1);
+
+  // event counter
+  se->registerSubsystem( new EventCounter_hp( "EVENTCOUNTER_HP", 1 ) );
+
+  // bbc reconstruction
+  BbcInit();
+  Bbc_Reco();
+
+  // tracking
+  Tracking_Cells();
+  Tracking_Clus();
+
+  // input manager
+  auto in = new Fun4AllDstInputManager("DSTin");
+  in->fileopen(inputFile);
+  se->registerInputManager(in);
+
+  // output manager
+  /* all the nodes from DST and RUN are saved to the output */
+  Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
+  se->registerOutputManager(out);
+
+  // process events
+  se->run(nEvents);
+
+  // terminate
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  gSystem->Exit(0);
+  return 0;
+}
+
+// This function is only used to test if we can load this as root6 macro
+// without running into unresolved libraries and include files
+void RunFFALoadTest() {}
