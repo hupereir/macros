2af563d61126523c354e0a95e7c92c1b540176fa
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-08-13 12:05:01 -0400

    - implemented skipEvents

--- detectors/sPHENIX/Fun4All_G4_Reconstruction_hp.C
+++ detectors/sPHENIX/Fun4All_G4_Reconstruction_hp.C
@@ -21,9 +21,16 @@ R__LOAD_LIBRARY(libqa_modules.so)
 //________________________________________________________________________________________________
 int Fun4All_G4_Reconstruction_hp(
   const int nEvents = 1,
+  const int nSkipEvents = 0,
   const char* inputFile = "DST/CONDOR_Hijing_Micromegas_50kHz/G4Hits_merged/G4Hits_sHijing_0-12fm_merged_000000_001000.root",
   const char *outputFile = "DST/Clusters.root" )
 {
+
+  // print inputs
+  std::cout << "Fun4All_G4_Reconstruction_hp - nEvents: " << nEvents << std::endl;
+  std::cout << "Fun4All_G4_Reconstruction_hp - nSkipEvents: " << nSkipEvents << std::endl;
+  std::cout << "Fun4All_G4_Reconstruction_hp - inputFile: " << inputFile << std::endl;
+  std::cout << "Fun4All_G4_Reconstruction_hp - outputFile: " << outputFile << std::endl;
 
   // central tracking
   Enable::MVTX = true;
@@ -31,6 +38,9 @@ int Fun4All_G4_Reconstruction_hp(
   Enable::TPC = true;
   Enable::TPC_ABSORBER = true;
   Enable::MICROMEGAS = true;
+
+  // micromegas configuration
+  G4MICROMEGAS::CONFIG = G4MICROMEGAS::CONFIG_Z_ONE_SECTOR;
 
   // tracking configuration
   G4TRACKING::use_track_prop = true;
@@ -47,7 +57,7 @@ int Fun4All_G4_Reconstruction_hp(
   // event counter
   se->registerSubsystem( new EventCounter_hp( "EventCounter_hp", 1 ) );
 
-  // cells 
+  // cells
   Mvtx_Cells();
   Intt_Cells();
   TPC_Cells();
@@ -58,14 +68,14 @@ int Fun4All_G4_Reconstruction_hp(
   Intt_Clustering();
   TPC_Clustering();
   Micromegas_Clustering();
-  
+
   if( false )
   {
     // tracking
     TrackingInit();
     Tracking_Reco();
   }
-  
+
   if( false )
   {
     // local evaluation
@@ -75,7 +85,7 @@ int Fun4All_G4_Reconstruction_hp(
       SimEvaluator_hp::EvalVertices|
       SimEvaluator_hp::EvalParticles );
     se->registerSubsystem(simEvaluator);
-    
+
     auto trackingEvaluator = new TrackingEvaluator_hp;
     trackingEvaluator->set_flags(
       TrackingEvaluator_hp::EvalEvent|
@@ -83,7 +93,7 @@ int Fun4All_G4_Reconstruction_hp(
       TrackingEvaluator_hp::EvalTracks);
     se->registerSubsystem(trackingEvaluator);
   }
-  
+
   // input manager
   auto in = new Fun4AllDstInputManager("DSTin");
   in->fileopen(inputFile);
@@ -93,6 +103,10 @@ int Fun4All_G4_Reconstruction_hp(
   /* all the nodes from DST and RUN are saved to the output */
   auto out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
   se->registerOutputManager(out);
+
+  // skip events if any specified
+  if( nSkipEvents > 0 )
+  { se->skip( nSkipEvents ); }
 
   // process events
   se->run(nEvents);
