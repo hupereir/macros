74295241f534a5d98b018d93a29db4bf38a016da
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-10-20 14:03:30 -0400

    updated evaluators

--- detectors/sPHENIX/Fun4All_G4_Reconstruction_hp.C
+++ detectors/sPHENIX/Fun4All_G4_Reconstruction_hp.C
@@ -11,6 +11,7 @@
 
 // own modules
 #include <g4eval/EventCounter_hp.h>
+#include <g4eval/MicromegasEvaluator_hp.h>
 #include <g4eval/SimEvaluator_hp.h>
 #include <g4eval/TrackingEvaluator_hp.h>
 
@@ -22,7 +23,7 @@ R__LOAD_LIBRARY(libqa_modules.so)
 //________________________________________________________________________________________________
 int Fun4All_G4_Reconstruction_hp(
   const int nEvents = 1,
-  const int nSkipEvents = 131,
+  const int nSkipEvents = 0,
   const char* inputFile = "DST/CONDOR_Hijing_Micromegas_50kHz/G4Hits_merged/G4Hits_sHijing_0-12fm_merged_000000_001000.root",
   const char *outputFile = "DST/dst_eval.root" )
 {
@@ -42,7 +43,9 @@ int Fun4All_G4_Reconstruction_hp(
 
   // TPC
   G4TPC::enable_distortions = true;
-  G4TPC::distortion_filename = "distortion_maps/BeamXingNBeamsx10.flat_B1.4_E-400.0.ross_phislice_lookup_r16xp36xz40.distortion_map.hist.root";
+  G4TPC::distortion_filename = "distortion_maps/fluct_average.rev3.1side.3d.file0.h_negz.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root";
+  // G4TPC::distortion_coordinates = PHG4TpcElectronDrift::COORD_PHI|PHG4TpcElectronDrift::COORD_R;
+  G4TPC::distortion_coordinates = PHG4TpcElectronDrift::COORD_PHI|PHG4TpcElectronDrift::COORD_R|PHG4TpcElectronDrift::COORD_Z;
 
   // micromegas configuration
   G4MICROMEGAS::CONFIG = G4MICROMEGAS::CONFIG_Z_ONE_SECTOR;
@@ -61,8 +64,8 @@ int Fun4All_G4_Reconstruction_hp(
   PHRandomSeed::Verbosity(1);
 
   // reco const
-  // auto rc = recoConsts::instance();
-  // rc->set_IntFlag("RANDOMSEED", 1);
+  auto rc = recoConsts::instance();
+  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
 
   // event counter
   se->registerSubsystem( new EventCounter_hp( "EventCounter_hp", 1 ) );
@@ -86,21 +89,35 @@ int Fun4All_G4_Reconstruction_hp(
     Tracking_Reco();
   }
 
-  if( true )
+  if( false )
   {
     // local evaluation
     auto simEvaluator = new SimEvaluator_hp;
     simEvaluator->set_flags(
-      SimEvaluator_hp::EvalEvent|
-      SimEvaluator_hp::EvalVertices|
-      SimEvaluator_hp::EvalParticles );
+      SimEvaluator_hp::EvalEvent
+      // |SimEvaluator_hp::EvalVertices
+      // |SimEvaluator_hp::EvalParticles
+      );
     se->registerSubsystem(simEvaluator);
+  }
 
+  if( false )
+  {
+    // Micromegas evaluation
+    auto micromegasEvaluator = new MicromegasEvaluator_hp;
+    micromegasEvaluator->set_flags( MicromegasEvaluator_hp::EvalG4Hits );
+    se->registerSubsystem(micromegasEvaluator);
+  }
+
+  if( true )
+  {
+    // tracking
     auto trackingEvaluator = new TrackingEvaluator_hp;
     trackingEvaluator->set_flags(
-      TrackingEvaluator_hp::EvalEvent|
-      TrackingEvaluator_hp::EvalClusters|
-      TrackingEvaluator_hp::EvalTracks);
+      TrackingEvaluator_hp::EvalEvent
+      // |TrackingEvaluator_hp::EvalClusters
+      |TrackingEvaluator_hp::EvalTracks
+      );
     se->registerSubsystem(trackingEvaluator);
   }
 
@@ -114,6 +131,7 @@ int Fun4All_G4_Reconstruction_hp(
   auto out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
   if( true )
   {
+    out->AddNode("MicromegasEvaluator_hp::Container");
     out->AddNode("SimEvaluator_hp::Container");
     out->AddNode("TrackingEvaluator_hp::Container");
   }
