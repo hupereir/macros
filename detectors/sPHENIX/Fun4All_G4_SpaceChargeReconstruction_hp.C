d4be18b35c3196b78439c131668c1da1b54cee78
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-11-13 12:15:28 -0500

    added space charge reconstruction code

---
+++ detectors/sPHENIX/Fun4All_G4_SpaceChargeReconstruction_hp.C
@@ -0,0 +1,107 @@
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <g4eval/TrackingEvaluator_hp.h>
+#include <g4main/PHG4VertexSelection.h>
+#include <phool/recoConsts.h>
+#include <tpccalib/TpcSpaceChargeReconstruction.h>
+
+
+#include <TSystemDirectory.h>
+
+// own modules
+#include <g4eval/EventCounter_hp.h>
+
+#include "G4_Tracking.C"
+
+R__LOAD_LIBRARY(libfun4all.so)
+
+//________________________________________________________________________________________________
+std::vector<TString> GetFiles( const char* pathname )
+{
+  std::vector<TString> filenames;
+
+ 	TSystemDirectory dir( pathname, pathname );
+  auto files = dir.GetListOfFiles();
+  TIter next( files );
+  while( auto file = static_cast<TSystemFile*>(next()) )
+  {
+    const TString filename( file->GetName() );
+    if( !filename.EndsWith( ".root" ) ) continue;
+    filenames.push_back( TString( pathname ) + filename );
+  }
+  return filenames;
+}
+
+//________________________________________________________________________________________________
+int Fun4All_G4_SpaceChargeReconstruction_hp(
+  const int nEvents = 0,
+  const char* inputDirectory = "DST/CONDOR_realistic_micromegas/dst_reco_truth_notpc_distortions-new/",
+  // const char* inputFile = "DST/CONDOR_realistic_micromegas/dst_reco_truth_notpc_distortions/dst_reco_realistic_micromegas_0.root",
+  const char* outputFile = "Rootfiles/Distortions_full_realistic_micromegas_mm-new.root"
+  )
+{
+
+  // print inputs
+  std::cout << "Fun4All_G4_SpaceChargeReconstruction_hp - nEvents: " << nEvents << std::endl;
+  // std::cout << "Fun4All_G4_SpaceChargeReconstruction_hp - inputFile: " << inputFile << std::endl;
+  std::cout << "Fun4All_G4_SpaceChargeReconstruction_hp - inputDirectory: " << inputDirectory << std::endl;
+  std::cout << "Fun4All_G4_SpaceChargeReconstruction_hp - outputFile: " << outputFile << std::endl;
+
+  // central tracking
+  Enable::MVTX = true;
+  Enable::INTT = true;
+  Enable::TPC = true;
+  Enable::TPC_ABSORBER = true;
+  Enable::MICROMEGAS = true;
+
+  // micromegas configuration
+  G4MICROMEGAS::CONFIG = G4MICROMEGAS::CONFIG_Z_ONE_SECTOR;
+
+  // tracking configuration
+  G4TRACKING::use_Genfit = true;
+  G4TRACKING::use_truth_track_seeding = false;
+  G4TRACKING::disable_mvtx_layers = false;
+  G4TRACKING::disable_tpc_layers = false;
+
+  // server
+  auto se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  // reco const
+  auto rc = recoConsts::instance();
+
+  // event counter
+  se->registerSubsystem( new EventCounter_hp( "EventCounter_hp", 1000 ) );
+
+  // space charge reconstruction
+  auto spaceChargeReconstruction = new TpcSpaceChargeReconstruction;
+  spaceChargeReconstruction->set_use_micromegas( true );
+  spaceChargeReconstruction->set_outputfile( outputFile );
+  // spaceChargeReconstruction->Verbosity(1);
+  se->registerSubsystem( spaceChargeReconstruction );
+
+  // input manager
+  auto in = new Fun4AllDstInputManager("DSTin");
+  auto files = GetFiles( "DST/CONDOR_realistic_micromegas/dst_reco_truth_notpc_distortions/" );
+  std::cout << "Fun4All_G4_SpaceChargeReconstruction_hp - file count: " << files.size() << std::endl;
+  for( const auto filename:files ) { in->AddFile( filename.Data() ); }
+
+  se->registerInputManager(in);
+
+  // process events
+  se->run(nEvents);
+
+  // terminate
+  se->End();
+  se->PrintTimer();
+  std::cout << "All done" << std::endl;
+  delete se;
+  gSystem->Exit(0);
+  return 0;
+}
+
+// This function is only used to test if we can load this as root6 macro
+// without running into unresolved libraries and include files
+void RunFFALoadTest() {}
