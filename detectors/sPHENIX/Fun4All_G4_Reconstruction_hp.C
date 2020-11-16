0676472d2556c29b79ddcef4869d8889011e72c5
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2020-11-16 12:05:22 -0500

    Use full distortion map, including static distortion

--- detectors/sPHENIX/Fun4All_G4_Reconstruction_hp.C
+++ detectors/sPHENIX/Fun4All_G4_Reconstruction_hp.C
@@ -44,7 +44,8 @@ int Fun4All_G4_Reconstruction_hp(
 
   // TPC
   G4TPC::ENABLE_DISTORTIONS = true;
-  G4TPC::distortion_filename = "distortion_maps/fluct_average.rev3.1side.3d.file0.h_negz.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root";
+  // G4TPC::distortion_filename = "distortion_maps/fluct_average.rev3.1side.3d.file0.h_negz.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root";
+  G4TPC::distortion_filename = "distortion_maps/average.rev3.1side.3d.file0.h_negz.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root";
   G4TPC::distortion_coordinates = PHG4TpcElectronDrift::COORD_PHI|PHG4TpcElectronDrift::COORD_R|PHG4TpcElectronDrift::COORD_Z;
 
   // micromegas configuration
