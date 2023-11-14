2b1f7158789bced4d523aa75241f7597e1d2e226
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-11-14 11:15:40 -0500

    nothing

--- detectors/sPHENIX/FindFile.C
+++ detectors/sPHENIX/FindFile.C
@@ -2,11 +2,18 @@
 
 #include <string>
 
+R__LOAD_LIBRARY(libFROG.so)
+
 void FindFile()
 {
   FROG frog;
-  const std::string short_filename = "DST_TRACKS_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000002-00000.root";
+  const std::string short_filename = "G4Hits_sHijing_0_20fm-0000000007-00000.root";
+  // const std::string short_filename = "DST_TRKR_HIT_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000007-00000.root";
+  // const std::string short_filename = "DST_TRKR_HIT_sHijing_0_20fm-0000000007-00000.root";
   const auto long_filename = frog.location( short_filename );
   std::cout << "short_filename: " << short_filename << std::endl;
   std::cout << "long_filename: " << long_filename << std::endl;
+  auto f = TFile::Open( long_filename );
+  auto tree = static_cast<TTree*>( f->Get("T") );
+  std::cout << "entries: " << tree->GetEntries() << std::endl;
 }
--- detectors/sPHENIX/Fun4All_G4_SimEvaluation_hp.C
+++ detectors/sPHENIX/Fun4All_G4_SimEvaluation_hp.C
@@ -5,28 +5,25 @@
 #include <phool/recoConsts.h>
 
 // own modules
-#include <g4eval/EventCounter_hp.h>
-#include <g4eval/MicromegasEvaluator_hp.h>
-#include <g4eval/SimEvaluator_hp.h>
+#include <g4eval_hp/EventCounter_hp.h>
+#include <g4eval_hp/MicromegasEvaluator_hp.h>
+#include <g4eval_hp/SimEvaluator_hp.h>
 
-#include "G4_Tracking.C"
+// local macros
+#include "G4Setup_sPHENIX.C"
+#include "G4_Global.C"
+#include "Trkr_RecoInit.C"
 
-// needed to avoid warnings at readback
-R__LOAD_LIBRARY(libg4bbc.so)
-R__LOAD_LIBRARY(libg4tpc.so)
-R__LOAD_LIBRARY(libg4intt.so)
-R__LOAD_LIBRARY(libg4mvtx.so)
-
-R__LOAD_LIBRARY(libg4eval.so)
+R__LOAD_LIBRARY(libtpc.so)
 R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libg4eval_hp.so)
 
 //________________________________________________________________________________________________
 int Fun4All_G4_SimEvaluation_hp(
     const int nEvents = 0,
     const int nSkipEvents = 0,
-    // const char* inputFile = "/phenix/u/hpereira/sphenix/work/g4simulations/DST/CONDOR_Hijing_Micromegas_50kHz/G4Hits_merged/3/G4Hits_sHijing_0-12fm_merged_000000_001000.root",
-    const char* inputFile = "/sphenix/data/data02/sphnxpro/MDC1/sHijing_HepMC/PileUp/data/DST_BBC_G4HIT_sHijing_0_12fm-0000000001-00000.root",
-    const char* outputFile = "DST/dst_simeval.root"
+    const char* inputFile = "G4Hits_sHijing_0_488fm-0000000007-00000.root",
+    const char* outputFile = "DST/dst_simeval_0_488fm-0000000007-00000.root"
 )
 {
 
@@ -36,35 +33,57 @@ int Fun4All_G4_SimEvaluation_hp(
   std::cout << "Fun4All_G4_SimEvaluation_hp - inputFile: " << inputFile << std::endl;
   std::cout << "Fun4All_G4_SimEvaluation_hp - outputFile: " << outputFile << std::endl;
 
+  // options
+  Enable::PIPE = true;
+  Enable::MBD = true;
+  Enable::MAGNET = true;
+  Enable::PLUGDOOR = false;
+
+  // enable all absorbers
+  // this is equivalent to the old "absorberactive" flag
+  Enable::ABSORBER = true;
+
+  // central tracking
+  Enable::MVTX = true;
+  Enable::INTT = true;
+  Enable::TPC = true;
   Enable::MICROMEGAS = true;
-  G4MICROMEGAS::CONFIG = G4MICROMEGAS::CONFIG_MAXIMAL;
+  Enable::BLACKHOLE = true;
 
   // server
   auto se = Fun4AllServer::instance();
   se->Verbosity(1);
 
-  auto rc = recoConsts::instance();
-  rc->set_IntFlag("RANDOMSEED", 1);
-
   // event counter
   se->registerSubsystem(new EventCounter_hp("EVENTCOUNTER_HP",1));
 
-  // need to run micromegas CELLS, in order to have a valid geometry
-  Micromegas_Cells();
+  // condition database
+  Enable::CDB = true;
+  auto rc = recoConsts::instance();
+  // rc->set_IntFlag("RANDOMSEED", 1);
+  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
+  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
+  
+//   G4Init();
+//   G4Setup();
+
+  TrackingInit();
+  
+//   // geometry initialization
+//   ACTSGEOM::ActsGeomInit();
+
+//   // need to run micromegas CELLS, in order to have a valid geometry
+//   Micromegas_Cells();
 
   // local evaluation
   auto simEvaluator = new SimEvaluator_hp;
-  simEvaluator->set_flags(
-    SimEvaluator_hp::EvalEvent|
-    SimEvaluator_hp::EvalVertices|
-    // SimEvaluator_hp::EvalHits|
-    SimEvaluator_hp::EvalParticles );
+  simEvaluator->set_flags( SimEvaluator_hp::EvalEvent );
   se->registerSubsystem(simEvaluator);
 
-//   // Micromegas evaluation
-//   auto micromegasEvaluator = new MicromegasEvaluator_hp;
-//   micromegasEvaluator->set_flags( MicromegasEvaluator_hp::EvalG4Hits );
-//   se->registerSubsystem(micromegasEvaluator);
+  // Micromegas evaluation
+  auto micromegasEvaluator = new MicromegasEvaluator_hp;
+  micromegasEvaluator->set_flags( MicromegasEvaluator_hp::EvalG4Hits );
+  se->registerSubsystem(micromegasEvaluator);
 
   // input manager
   auto in = new Fun4AllDstInputManager("DSTin");
@@ -73,7 +92,7 @@ int Fun4All_G4_SimEvaluation_hp(
 
   // output manager
   auto out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
-  out->AddNode("SimEvaluator_hp::Container");
+  // out->AddNode("SimEvaluator_hp::Container");
   out->AddNode("MicromegasEvaluator_hp::Container");
   se->registerOutputManager(out);
 
