9fa1b75e8c8673f11b5063d275ab38779cb912b5
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2025-03-05 08:53:50 -0500

    nothing

--- detectors/sPHENIX/FindFile.C
+++ detectors/sPHENIX/FindFile.C
@@ -7,7 +7,7 @@ R__LOAD_LIBRARY(libFROG.so)
 void FindFile()
 {
   FROG frog;
-  const std::string short_filename = "G4Hits_sHijing_0_20fm-0000000007-00000.root";
+  const std::string short_filename = "pedestal-54256-01982.root";
   // const std::string short_filename = "DST_TRKR_HIT_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000007-00000.root";
   // const std::string short_filename = "DST_TRKR_HIT_sHijing_0_20fm-0000000007-00000.root";
   const auto long_filename = frog.location( short_filename );
