b2da7db2bfd97c9567fd9ce030435a37a7c07073
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2025-12-03 12:38:06 -0500

    removed obsolete includes.

--- detectors/sPHENIX/Fun4All_G4_Simulation_hp.C
+++ detectors/sPHENIX/Fun4All_G4_Simulation_hp.C
@@ -14,7 +14,6 @@
 #include <g4eval_hp/SimEvaluator_hp.h>
 #include <g4eval_hp/MicromegasEvaluator_hp.h>
 #include <g4eval_hp/TrackingEvaluator_hp.h>
-#include <tpccalib/TpcSpaceChargeReconstruction.h>
 
 // local macros
 #include "G4Setup_sPHENIX.C"
@@ -50,7 +49,7 @@ int Fun4All_G4_Simulation_hp(
   Enable::TPC = true;
   Enable::MICROMEGAS = true;
   Enable::BLACKHOLE = true;
-   
+
   // server
   auto se = Fun4AllServer::instance();
   se->Verbosity(1);
@@ -81,7 +80,7 @@ int Fun4All_G4_Simulation_hp(
       const std::vector<double> pt_bins = {0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.5, 3.8, 4, 4.4, 4.8, 5.2, 5.6, 6, 6.5, 7, 8, 9, 10};
       const std::vector<double> yield_int = {2.23, 1.46, 0.976, 0.663, 0.457, 0.321, 0.229, 0.165, 0.119, 0.0866, 0.0628, 0.0458, 0.0337, 0.0248, 0.0183, 0.023, 0.0128, 0.00724, 0.00412, 0.00238, 0.00132, 0.00106, 0.000585, 0.00022, 0.000218, 9.64e-05, 4.48e-05, 2.43e-05, 1.22e-05, 7.9e-06, 4.43e-06, 4.05e-06, 1.45e-06, 9.38e-07};
       gen->set_pt_range(pt_bins,yield_int);
-    } else 
+    } else
     {
       // flat pt distribution
       gen->set_pt_range(0.2, 50.0);
@@ -115,11 +114,11 @@ int Fun4All_G4_Simulation_hp(
     Mvtx_Cells();
     Intt_Cells();
     TPC_Cells();
-  
+
     if( Enable::MICROMEGAS )
-    { Micromegas_Cells(); }  
+    { Micromegas_Cells(); }
   }
-  
+
   // digitizer and clustering
   if( false )
   {
@@ -129,15 +128,15 @@ int Fun4All_G4_Simulation_hp(
     if( Enable::MICROMEGAS )
     { Micromegas_Clustering(); }
   }
-  
-  if( false ) 
+
+  if( false )
   {
     // Micromegas evaluation
     auto micromegasEvaluator = new MicromegasEvaluator_hp;
-    micromegasEvaluator->set_flags( 
+    micromegasEvaluator->set_flags(
       MicromegasEvaluator_hp::EvalG4Hits
-      |MicromegasEvaluator_hp::EvalHits 
-      |MicromegasEvaluator_hp::PrintGeometry 
+      |MicromegasEvaluator_hp::EvalHits
+      |MicromegasEvaluator_hp::PrintGeometry
      );
     se->registerSubsystem(micromegasEvaluator);
   }
@@ -154,7 +153,7 @@ int Fun4All_G4_Simulation_hp(
     simEvaluator->set_flags( SimEvaluator_hp::EvalHits );
     se->registerSubsystem(simEvaluator);
   }
-  
+
   // for single particle generators we just need something which drives
   // the event loop, the Dummy Input Mgr does just that
   auto in = new Fun4AllDummyInputManager("JADE");
